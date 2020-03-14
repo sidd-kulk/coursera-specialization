51b67b261ac8e31f4af5525c38988f2c10d64dcc
Siddharth <sidcool1234@gmail.com>
2020-03-14 17:20:04 +0530

    Adding new implementation for Fibonacci

--- src/main/kotlin/sid/coursera/week2/Fibonacci.kt
+++ src/main/kotlin/sid/coursera/week2/Fibonacci.kt
@@ -2,18 +2,30 @@ package sid.coursera.week2
 
 
 
-fun fibonacci(till: Int): Int {
+fun fibonacciFirstN(firstN: Int): Int {
     val cache = mutableMapOf<Int, Int>()
 
-    fun fibonacci(till: Int, cache: Map<Int, Int>): Int{
-        if(till <= 1) return till;
+    fun fibonacci(firstN: Int, cache: Map<Int, Int>): Int{
+        if(firstN <= 1) return firstN;
 
-        val fibTill1: Int = cache[till - 1] ?: fibonacci(till - 1, cache)
-        val fibTill2: Int = cache[till - 2] ?: fibonacci(till - 2, cache)
+        val fibTill1: Int = cache[firstN - 1] ?: fibonacci(firstN - 1, cache)
+        val fibTill2: Int = cache[firstN - 2] ?: fibonacci(firstN - 2, cache)
 
         return fibTill1 + fibTill2
     }
 
-    return fibonacci(till, cache)
+    return fibonacci(firstN, cache)
+}
 
+fun fibonacciTill(till: Int): Int {
+    var fibNMinus1 = 1
+
+    var fibN = 1
+    while(fibN + fibNMinus1 < till){
+        val temp = fibN
+        fibN += fibNMinus1
+        fibNMinus1 = temp
+    }
+
+    return fibN
 }
--- src/test/kotlin/sid/coursera/week2/FibonacciTest.kt
+++ src/test/kotlin/sid/coursera/week2/FibonacciTest.kt
@@ -5,22 +5,53 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class FibonacciTest {
     @Test
+    fun `5th Fibonacci is 3`() {
+        assertEquals(5, fibonacciFirstN(5))
+    }
+
+    @Test
+    fun `10th Fibonacci is 55`() {
+        assertEquals(55, fibonacciFirstN(10))
+    }
+
+    @Test
+    fun `20th Fibonacci is 6365`() {
+        assertEquals(6765, fibonacciFirstN(20))
+    }
+
+    @Test
+    fun `30th Fibonacci is 832040`() {
+        assertEquals(832040, fibonacciFirstN(30))
+    }
+
+    // --------------------------------//
+
+    @Test
     fun `Fibonacci till 5 is 3`() {
-        assertEquals(5, fibonacci(5))
+        assertEquals(3, fibonacciTill(5))
     }
 
     @Test
-    fun `Fibonacci till 10 is 55`() {
-        assertEquals(55, fibonacci(10))
+    fun `Fibonacci till 10 is 8`() {
+        assertEquals(8, fibonacciTill(10))
     }
 
     @Test
-    fun `Fibonacci till 20 is 6365`() {
-        assertEquals(6765, fibonacci(20))
+    fun `Fibonacci till 20 is 6765`() {
+        assertEquals(6765, fibonacciFirstN(20))
     }
 
     @Test
     fun `Fibonacci till 30 is 832040`() {
-        assertEquals(832040, fibonacci(30))
+        assertEquals(832040, fibonacciFirstN(30))
+    }
+
+}
+
+internal class PerformanceTest {
+    @Test
+    fun `Performance Testing`(){
+        // TODO
+
     }
 }
