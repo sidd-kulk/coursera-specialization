e7576c65f028b8aa3544cc23a9ec849bcbb18727
Siddharth <sidcool1234@gmail.com>
2020-03-15 10:42:02 +0530

    Implementing Euclidean algorithm for GCD

--- src/main/kotlin/sid/coursera/week2/GreatestCommonDivisor.kt
+++ src/main/kotlin/sid/coursera/week2/GreatestCommonDivisor.kt
@@ -1,6 +1,11 @@
 package sid.coursera.week2
 
 fun greatestCommonDivisor(a: Int, b: Int): Int {
-    val loopCount = if (a > b)  b else a // Take the smallest amont the two numbers
+    val loopCount = minOf(a, b) // Take the smallest amont the two numbers
     return (1 .. loopCount).filter { a % it == 0 && b % it == 0}.max()!!
+}
+
+fun euclideanAlgorithm(a: Int, b: Int): Int {
+    if (b == 0) return a
+    return euclideanAlgorithm(b, a % b)
 }
--- src/test/kotlin/sid/coursera/week2/GreatestCommonDivisorKtTest.kt
+++ src/test/kotlin/sid/coursera/week2/GreatestCommonDivisorKtTest.kt
@@ -7,9 +7,20 @@ import org.junit.jupiter.api.Assertions.*
 internal class GreatestCommonDivisorKtTest {
 
     @Test
-    fun `GCD Tests`() {
+    fun `GCD Tests for brute force`() {
         assertEquals(2, greatestCommonDivisor(4, 10))
         assertEquals(4, greatestCommonDivisor(8, 20))
         assertEquals(81, greatestCommonDivisor(81, 729))
+        assertEquals(61232, greatestCommonDivisor(3918848, 1653264))
+        assertEquals(61232, greatestCommonDivisor(1653264, 3918848))
+    }
+
+    @Test
+    fun `GCD Tests for euclidean algorithm`() {
+        assertEquals(2, euclideanAlgorithm(4, 10))
+        assertEquals(4, euclideanAlgorithm(8, 20))
+        assertEquals(81, euclideanAlgorithm(81, 729))
+        assertEquals(61232, euclideanAlgorithm(3918848, 1653264))
+        assertEquals(61232, euclideanAlgorithm(1653264, 3918848))
     }
 }
