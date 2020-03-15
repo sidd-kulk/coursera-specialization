65d0837c4e8f34cafe19784a23df243a05692138
Siddharth <sidcool1234@gmail.com>
2020-03-15 10:27:39 +0530

    Implementing functional solution to GCD problem

--- src/main/kotlin/sid/coursera/week2/GreatestCommonDivisor.kt
+++ src/main/kotlin/sid/coursera/week2/GreatestCommonDivisor.kt
@@ -1,14 +1,6 @@
 package sid.coursera.week2
 
 fun greatestCommonDivisor(a: Int, b: Int): Int {
-    val loopCount = if (a > b)  a else b
-
-    var gcd = 1
-
-    for(i in 1 .. loopCount) {
-        if(a % i == 0 && b % i == 0) {
-            gcd = i
-        }
-    }
-    return gcd
+    val loopCount = if (a > b)  b else a // Take the smallest amont the two numbers
+    return (1 .. loopCount).filter { a % it == 0 && b % it == 0}.max()!!
 }
