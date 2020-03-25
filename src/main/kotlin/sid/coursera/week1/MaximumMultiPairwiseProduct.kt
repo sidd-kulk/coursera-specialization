2b8767ddfc98cbec88deadd2ea297a371b172453
Siddharth <sidcool1234@gmail.com>
2020-03-25 11:18:55 +0530

    Implementing generic solution for pair wise product

--- src/main/kotlin/sid/coursera/week1/MaximumMultiPairwiseProduct.kt
+++ src/main/kotlin/sid/coursera/week1/MaximumMultiPairwiseProduct.kt
@@ -1,12 +1,35 @@
 package sid.coursera.week1
 
 import java.lang.IllegalArgumentException
+import kotlin.math.abs
 
-class MaximumMultiPairwiseProduct (private val array: Array<Int>, private val take: Int): Problem<Long> {
+class MaximumMultiPairwiseProduct(private val array: Array<Int>, private val take: Int) : Problem<Long> {
 
     override fun solve(): Long {
-        if(array.size < take)  throw IllegalArgumentException("Check your parameters")
+        checkInput()
 
-        return array.map {it.toLong()}.sorted().takeLast(take).reduce {acc, i -> i * acc}
+        val multiplierIndex = take - 1
+        var max: Long = 0
+
+        val sortedArrayWithAbsoluteValue = array.sortedWith(absoluteValueComparator())
+
+        val product = sortedArrayWithAbsoluteValue
+            .map { it.toLong() }
+            .takeLast(multiplierIndex).reduce { acc, ele -> acc * ele }
+
+        (sortedArrayWithAbsoluteValue.lastIndex - multiplierIndex downTo 0).forEach { i ->
+            val productForIndex = product * sortedArrayWithAbsoluteValue[i]
+            max = if (max <= productForIndex) productForIndex else max
+        }
+
+        return max
+    }
+
+    private fun absoluteValueComparator() = Comparator<Int> { i, j -> abs(i) - abs(j) }
+
+    @Throws(IllegalArgumentException::class)
+    private fun checkInput() {
+        if (array.isNullOrEmpty() || array.size < take || take <= 0)
+            throw IllegalArgumentException("Check your parameters")
     }
 }
--- src/test/kotlin/sid/coursera/week1/MaximumMultiPairwiseProductTest.kt
+++ src/test/kotlin/sid/coursera/week1/MaximumMultiPairwiseProductTest.kt
@@ -19,9 +19,21 @@ internal class MaximumMultiPairwiseProductTest {
         assertEquals(3000000000000, problem.solve())
     }
 
+    @Test
+    fun `Maximum Pairwise Product Negative numbers`() {
+        val smallProblem = MaximumMultiPairwiseProduct(arrayOf(-5, -2, 8, 0, -9), 2)
+        val largeProblem = MaximumMultiPairwiseProduct(arrayOf(-5, -2, 8, 0, -9, 8, -21, 5 -6, 8, 12), 6)
+
+        assertEquals(45, smallProblem.solve())
+        assertEquals(1161216, largeProblem.solve())
+    }
+
     @Test()
-    fun `fails if take greater than array size`() {
-        val problem = MaximumMultiPairwiseProduct(arrayOf(2), 2)
-        assertThrows(IllegalArgumentException::class.java) {problem.solve()}
+    fun `fails if take greater than array size or when take is zero or less or when array is empty`() {
+        arrayOf(MaximumMultiPairwiseProduct(arrayOf(2), 3),
+                MaximumMultiPairwiseProduct(arrayOf(2), -2),
+                MaximumMultiPairwiseProduct(arrayOf(), 2))
+                    .forEach { assertThrows(IllegalArgumentException::class.java) {it.solve()}}
+
     }
 }
